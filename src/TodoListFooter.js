22712714f6fc5405cd34f92852781d4d09cd6c78
PRoman93 <roman_prokopets@mail.ua>
2020-02-25 15:51:32 +0200

    1-st todolist is done

---
+++ src/TodoListFooter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoListFooter = (props) => {
+    return (
+        <div className="todoList-footer">
+            <button>All</button>
+            <button>Completed</button>
+            <button>Active</button>
+        </div>
+    )
+}
+export default TodoListFooter
