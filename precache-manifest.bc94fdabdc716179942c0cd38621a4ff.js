2261fa65615f67fbfeb42651c7730a6ce2dd6638
PRoman93 <roman_prokopets@mail.ua>
2020-02-25 15:20:24 +0200

    Updates

---
+++ precache-manifest.bc94fdabdc716179942c0cd38621a4ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f670ea5a84021eafe576537045d8377",
+    "url": "/mytodolist/index.html"
+  },
+  {
+    "revision": "f0fd06c263ca2eb2e21a",
+    "url": "/mytodolist/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5692f5e6745a2d39112d",
+    "url": "/mytodolist/static/js/2.811c94e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mytodolist/static/js/2.811c94e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0fd06c263ca2eb2e21a",
+    "url": "/mytodolist/static/js/main.01ad90f5.chunk.js"
+  },
+  {
+    "revision": "a020d68ef4f5f440620b",
+    "url": "/mytodolist/static/js/runtime-main.9560ea7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mytodolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
